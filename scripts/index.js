4a5bc66d1c7c4ba954033a5aff3c79861b2beaa9
cdobbi <dob24001@byui.edu>
2025-01-07 12:20:53 -0700

    ...

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenu = document.querySelector(".hamburger-menu");
   const navLinks = document.querySelector(".nav-links");
