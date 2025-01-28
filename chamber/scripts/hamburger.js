d824462a97d42c9dc6b5230de4d03237be5e24d9
cdobbi <dob24001@byui.edu>
2025-01-28 13:46:44 -0700

    ...

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -22,5 +22,34 @@ document.addEventListener("DOMContentLoaded", () => {
             hamburger.setAttribute("aria-label", "Open navigation"));
         });
       });
+});
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links a");
+    const mainContent = document.querySelector("main");
+  
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+      mainContent.classList.toggle("shifted");
+      const isActive = navLinks.classList.contains("active");
+      hamburger.setAttribute("aria-expanded", isActive);
+      hamburger.setAttribute(
+        "aria-label",
+        isActive ? "Close navigation" : "Open navigation"
+      );
+    });
+  
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        if (navLinks.classList.contains("active")) {
+          navLinks.classList.remove("active");
+          mainContent.classList.remove("shifted");
+          hamburger.setAttribute("aria-expanded", "false");
+          hamburger.setAttribute("aria-label", "Open navigation");
+        }
+      });
+    });
   });
   
