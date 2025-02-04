2df5d89fce7243daabf00a3b9fbc3650f959e0d8
cdobbi <dob24001@byui.edu>
2025-02-04 10:21:03 -0700

    changed color scheme and added favicon and logo

--- week05/scripts/getdates.js
+++ week05/scripts/getdates.js
@@ -1,3 +1,18 @@
-document.getElementById("lastModified").textContent = document.lastModified;
-
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+    const yearElement = document.querySelector("#year");
+    const lastModifiedElement = document.querySelector("#lastModified");
+  
+    if (yearElement) {
+      yearElement.textContent = new Date().getFullYear();
+    }
+  
+    if (lastModifiedElement) {
+      const lastModifiedDate = new Date(document.lastModified);
+      const formatter = new Intl.DateTimeFormat("en-US", {
+        timeStyle: "medium",
+        dateStyle: "medium",
+      });
+      lastModifiedElement.textContent = formatter.format(lastModifiedDate);
+    }
+  });
+  
