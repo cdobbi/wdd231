12d085faada1320a9aec770a71ab4a9f979192a2
cdobbi <dob24001@byui.edu>
2025-02-14 10:39:01 -0700

    working hambuger and nav links

--- quilt/scripts/quilt.js
+++ quilt/scripts/quilt.js
@@ -59,3 +59,21 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+import { fetchQuiltPatterns } from "./patterns.js";
+import { handleFormSubmission } from "./formHandler.js";
+import { toggleModal, closeModal } from "./modal.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchQuiltPatterns();
+
+  document
+    .getElementById("form")
+    .addEventListener("submit", handleFormSubmission);
+
+  document.querySelectorAll(".quilt-image").forEach((image) => {
+    image.addEventListener("click", () => toggleModal(image.src));
+  });
+
+  document.getElementById("close-modal").addEventListener("click", closeModal);
+});
